bc4c9d4ac7f6d37c41a8cd678db8a83e64ef7fb6
Salamun Nuhin <52977980+Sal47506@users.noreply.github.com>
2025-10-29 07:15:56 -0400

    makefile made and other issues addressed

--- supply-demand-signals-2/header/Person.h
+++ supply-demand-signals-2/header/Person.h
@@ -1,23 +1,28 @@
 #pragma once 
+#include "Product.h"
 #include <string>
 #include <vector>
 #include <unordered_map>
 #include <random>
 
-enum HEALTH_STATUS { HEALTHY, UNHEALTHY, RECOVERING };
+
+
+enum health_status { HEALTHY, UNHEALTHY, RECOVERING };
+
+enum expertise { EXPERTISE_1, EXPERTISE_2, EXPERTISE_3 };
 
 
 struct Person {
   std::unordered_map<std::string, int> expertise;
   int age;
-  HEALTH_STATUS healthStatus;
-  std::unordered_map<std::string, int> needs;
+  health_status healthStatus;
+  std::unordered_map<std::string, double> needs;
 
   
-  Person(const std::unordered_map<std::string, int>& expertise, int age, HEALTH_STATUS healthStatus, const std::unordered_map<std::string, int>& needs) : expertise(expertise), age(age), healthStatus(healthStatus), needs(needs) {}
+  Person(const std::unordered_map<std::string, int>& expertise, int age, health_status healthStatus, const std::unordered_map<std::string, double>& needs) : expertise(expertise), age(age), healthStatus(healthStatus), needs(needs) {}
   
-  std::unordered_map<std::string, int>& getWorkerNeeds() {return this->needs;};
-  HEALTH_STATUS getHealthStatus() {return this->healthStatus;};
+  std::unordered_map<std::string, double>& getWorkerNeeds() {return this->needs;};
+  health_status getHealthStatus() {return this->healthStatus;};
 
   float getCurrentProductivity();
   
