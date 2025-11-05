34d4bcf2d10b8bcdf69b6bd7e185836740cff5c4
Salamun Nuhin <52977980+Sal47506@users.noreply.github.com>
2025-11-05 15:04:03 -0500

    minor changes to Distributor

--- supply-demand-signals-2/src/Distributor.cpp
+++ supply-demand-signals-2/src/Distributor.cpp
@@ -12,3 +12,7 @@ double Distributor::get_output_ratio(Product& product) {
 double Distributor::planned_satisfaction_per_person(Product& product, Person& person) {
     return get_output_ratio(product) * person.get_worker_needs()[product.product_name];
 }
+
+void Distributor::sell_goods(Product& product, int quantity) {
+    inventory[product].second = (inventory.find(product) == inventory.end() && inventory[product].second > quantity) ? inventory[product].second - quantity : inventory[product].second;
+}
