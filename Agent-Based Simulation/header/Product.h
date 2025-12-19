ff072e2548fffad2d61c575fffbdd464d24b9309
Amittai Aviram <aviram@bc.edu>
2025-12-19 17:18:32 -0500

    Set initial product inputs and quantities thereof

--- Agent-Based Simulation/header/Product.h
+++ Agent-Based Simulation/header/Product.h
@@ -10,7 +10,7 @@ struct Machine;
 
 struct Product {
     Product(const std::string name);
-    void set_inputs(std::vector<Product *>& products, int product_index);
+    void set_inputs(std::vector<Product *>& products);
     std::string product_name;
     double price_per_unit;
     int order_size;
