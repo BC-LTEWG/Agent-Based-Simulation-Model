b555678357b054324615dda4dff887aa22655ab3
Amittai-Aviram <amittai.aviram@gmail.com>
2025-12-27 02:26:30 -0500

    Use smart pointers; eliminate emtries below pivot

--- Agent-Based Simulation/src/leontief_function.cpp
+++ Agent-Based Simulation/src/leontief_function.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 
 #include "leontief_function.h"
@@ -15,13 +16,22 @@ void transpose(Matrix * matrix_a) {
 void reduced_row_echelon(Matrix * matrix) {
     for (size_t i = 0; i < matrix->size() - 1; ++i) {
         if (!(*(*matrix)[i])[i]) {
-            for (size_t j = i; j < matrix->size(); ++j) {
-                if ((*(*matrix)[i])[j]) {
-                    std::swap(matrix[i], matrix[j]);
+            size_t j = i;
+            for (size_t k = j; k < matrix->size(); ++k) {
+                if ((*(*matrix)[k])[j]) {
+                    std::swap((*matrix)[k], (*matrix)[i]);
+                    break;
                 }
             }
         }
-        if ((*(*matrix)[i])[i]) {
+        double pivot = (*(*matrix)[i])[i];
+        if (pivot) {
+            for (size_t k = i + 1; k < matrix->size(); ++k) {
+                double pivot_row_multiplier = (*(*matrix)[k])[i] / pivot;
+                for (size_t j = i; j < matrix->size(); ++j) {
+                    (*(*matrix)[k])[j] -= pivot_row_multiplier * (*(*matrix)[i])[j];
+                }
+            }
         }
     }
 }
