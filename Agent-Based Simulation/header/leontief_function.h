5119baecf4eab31db5c4f20c38c663ce35184368
Amittai-Aviram <amittai.aviram@gmail.com>
2025-12-28 02:11:13 -0500

    Add inverse and supporting functions (but not working yet)

--- Agent-Based Simulation/header/leontief_function.h
+++ Agent-Based Simulation/header/leontief_function.h
@@ -5,4 +5,7 @@ typedef std::vector<double> Row;
 typedef std::vector<std::unique_ptr<Row>> Matrix;
 
 void transpose(Matrix *);
-void reduced_row_echelon(Matrix *);
+void reduced_row_echelon_form(Matrix *, size_t, size_t);
+void augment(Matrix * matrix);
+std::unique_ptr<Matrix> inverse(Matrix *);
+void round_to_zero(Matrix *, size_t, size_t);
