c576c244bef640d70c7360a15b4bf95aa3483557
Amittai-Aviram <amittai.aviram@gmail.com>
2026-01-02 17:59:22 -0600

    Restore Devin's version of Person.cpp

--- Agent-Based Simulation/src/Person.cpp
+++ Agent-Based Simulation/src/Person.cpp
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <cstdlib>
 #include <cmath>
-#include <iostream>
 #include <random>
 
 #include "Constants.h"
@@ -40,34 +39,13 @@ Person::Person():
                 ranked_distributors.end(),
                 Sim::gen
                 );
-    shopping_offset =
-        (((int) shopping_dist(Sim::gen)) +
-         PERSON_SHOPPING_PERIOD) % PERSON_SHOPPING_PERIOD;
-
-    static std::normal_distribution<>
-        ability_dist(1.0, PERSON_ABILITY_STDDEV);
-    std::vector<Ability> all_abilities;
-    for (int i = 0; i < NUM_ABILITIES; i++) {
-        all_abilities.push_back((Ability) i);
+        static std::normal_distribution<>
+            dist(1, PERSON_FREQUENCY_MULTIPLIER_STDDEV);
+        for (Product * p : Society::instance->get_products()) {
+            purchase_frequencies[p] =
+                p->mean_consumption_frequency * std::abs(dist(Sim::gen));
+        }
     }
-    std::shuffle(all_abilities.begin(), all_abilities.end(), Sim::gen);
-    all_abilities.resize(PERSON_ABILITY_COUNT_MAX);
-    for (Ability ability : all_abilities) {
-        abilities[ability] = std::max(0.0, ability_dist(Sim::gen));
-    }
-    ranked_distributors = Society::instance->get_distributors();
-    std::shuffle(
-            ranked_distributors.begin(),
-            ranked_distributors.end(),
-            Sim::gen
-            );
-    static std::normal_distribution<>
-        dist(1, PERSON_FREQUENCY_MULTIPLIER_STDDEV);
-    for (Product * p : Society::instance->get_products()) {
-        purchase_frequencies[p] =
-            p->mean_consumption_frequency * std::abs(dist(Sim::gen));
-    }
-}
 
 std::unordered_map<Person::Ability, double>& Person::get_abilities() {
     return this->abilities;
@@ -97,14 +75,14 @@ Person::HealthStatus Person::get_health_status() {
 }
 
 float Person::get_current_productivity() {
-    switch(health_status) {
-        case HEALTHY:
-            return 1.0;
-        case UNHEALTHY:
-            return UNHEALTHY_PRODUCTIVITY;
-        default:
-            return 1.0;
-    }
+	switch(health_status) {
+		case HEALTHY:
+			return 1.0;
+		case UNHEALTHY:
+			return UNHEALTHY_PRODUCTIVITY;
+		default:
+			return 1.0;
+	}
 }
 
 float Person::avg_productivity_over_time_step(std::string product_name) {
@@ -122,16 +100,13 @@ void Person::purchase_good(Product * p, int quantity) {
 }
 
 bool Person::will_shop() {
-    return Sim::get_current_time_step() % PERSON_SHOPPING_PERIOD ==
-        shopping_offset;
+    return Sim::get_current_time_step() % PERSON_SHOPPING_PERIOD == shopping_offset;
 }
 
 void Person::shop() {
-    static std::normal_distribution<>
-        dist(1, PERSON_SHOPPING_MULTIPLIER_STDDEV);
+    static std::normal_distribution<> dist(1, PERSON_SHOPPING_MULTIPLIER_STDDEV);
     for (auto &p : purchase_frequencies) {
-        int quantity = std::round(p.second * PERSON_SHOPPING_PERIOD *
-                std::abs(dist(Sim::gen)));
+        int quantity = std::round(p.second * PERSON_SHOPPING_PERIOD * std::abs(dist(Sim::gen)));
         if (quantity > 0) {
             purchase_good(p.first, quantity);
         }
@@ -149,22 +124,22 @@ void Person::retire() {
 }
 
 void Person::update_health_status() {
-    static std::uniform_real_distribution<> dist(0, 1);
-    if (health_status == HEALTHY &&
-            dist(Sim::gen) < 1 - pow(1 - DAILY_SICKNESS_CHANCE, 1.0 / DAY)) {
-        health_status = UNHEALTHY;
-    } else if (health_status == UNHEALTHY &&
-            dist(Sim::gen) < 1 - pow(1 - DAILY_RECOVERY_CHANCE, 1.0 / DAY)) {
-        health_status = HEALTHY;
-    } 
+	static std::uniform_real_distribution<> dist(0, 1);
+	if (health_status == HEALTHY &&
+		dist(Sim::gen) < 1 - pow(1 - DAILY_SICKNESS_CHANCE, 1.0 / DAY)) {
+		health_status = UNHEALTHY;
+	} else if (health_status == UNHEALTHY &&
+	   dist(Sim::gen) < 1 - pow(1 - DAILY_RECOVERY_CHANCE, 1.0 / DAY)) {
+		health_status = HEALTHY;
+	} 
 }
 
 
 void Person::on_time_step() {
-    ++age;
-    if (will_shop()) { shop(); }
-    if (will_retire()) { retire(); }
-    update_health_status();
+	++age;
+	if (will_shop()) { shop(); }
+	if (will_retire()) { retire(); }
+	update_health_status();
 }
 
 void Person::set_firm(Firm * workplace) {
