a7d081e8004cffe22b3c99d19317a6a1c3a1928b
Amittai-Aviram <amittai.aviram@gmail.com>
2026-01-17 17:11:50 -0500

    Adjust product price and average labor time at plan completion.

--- Agent-Based Simulation/header/PriceController.h
+++ Agent-Based Simulation/header/PriceController.h
@@ -1,31 +1,9 @@
 #pragma once
-#ifndef PRICE_CONTROLLER_H
-#define PRICE_CONTROLLER_H
-
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-
-#include "Agent.h"
-#include "Constants.h"
 
 struct Plan;
-struct Product;
-class Firm;
 
-class PriceController : public Agent {
+class PriceController {
     public:
-        void on_time_step() override;
-        static void update_prices();
-        static double get_price(Product * product);
-        static void set_price(Product * product, double price);
-        static void get_all_prices();
-        static void set_all_prices(std::unordered_map<Product *, double> prices);
-        static void update_plan(Plan * plan);
-        static void get_plan_history();
-    private:
-        static inline std::unordered_map<Product *, double> price_history;
-        static inline std::vector<Plan *> plan_history;
+        static void update_price(Plan *);
 };
 
-#endif
