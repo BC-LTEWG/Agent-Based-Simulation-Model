f11661d99b16df5177e7840524622ca4fa172bbd
Amittai-Aviram <amittai.aviram@gmail.com>
2026-02-14 12:30:25 -0500

    Compute new price based on overages over time window

--- Agent-Based Simulation/src/PriceController.cpp
+++ Agent-Based Simulation/src/PriceController.cpp
@@ -3,6 +3,7 @@
 #include "Constants.h"
 #include "Distributor.h"
 #include "Firm.h"
+#include "Logger.h"
 #include "Machine.h"
 #include "PriceController.h"
 #include "Product.h"
@@ -22,27 +23,28 @@ void PriceController::update_price(Plan * plan) {
     if (plan_history[product].begin()->second <= end_time) {
         plan_history[product].erase(plan_history[product].begin());
     }
-    int past_total_units = 0;
-    int num_workers = 0;
-    for (std::pair<Plan *, int> entry : plan_history[product]) {
-        past_total_units += entry.first->order->quantity;
-        num_workers += entry.first->workers.size();
-    }
-    int total_units = past_total_units + plan->order->quantity;
-    num_workers += plan->workers.size();
-    double past_total_labor = product->living_labor_per_unit * past_total_units;
-    double new_actual_labor =
-        plan->labor_hours - plan->labor_hours_remaining;
-    double average_labor_per_unit =
-        (new_actual_labor + past_total_labor) / total_units;
     plan_history[product].push_back(std::make_pair(plan, now));
-    double price = product->living_labor_per_unit = average_labor_per_unit;
+    int units = 0;
+    double hours = 0.0;
+    int workers = 0;
+    for (std::pair<Plan *, int> entry : plan_history[product]) {
+        Plan * plan = entry.first;
+        units += plan->order->quantity;
+        hours += plan->labor_hours - plan->labor_hours_remaining;
+        workers += plan->workers.size();
+    }
+    double price = product->living_labor_per_unit = hours / units;
     for (std::pair<Product *, double> input : product->inputs_per_unit) {
         price += input.first->price_per_unit * input.second;
     }
+    double machine_use_hours = hours / workers;
+    double machine_hours_per_unit = machine_use_hours / units;
     for (Machine * machine : product->machines_needed) {
-        price += (machine->price_per_unit / machine->lifetime) * average_labor_per_unit;
+        double machine_cost_per_hour =
+            machine->price_per_unit / machine->lifetime;
+        price += machine_cost_per_hour * machine_hours_per_unit;
     }
     product->price_per_unit = price;
+    Logger::get_instance()->log(Logger::SOCIETY, "price", product->id, price);
 }
 
