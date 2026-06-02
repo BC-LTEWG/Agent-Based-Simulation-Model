9db9766896d78a37a05933b2f298420f73ed19de
Nathan Lim <limnat@bc.edu>
2026-06-02 14:52:34 -0400

    Fix null keys, segfaults, and division-by-zero cases (#212)
    * Safety audit: memory, null pointers, divison-by-zero, datatypes
    
    * revert to main
    
    * Fix memory and logic in Firm/Producer (#191)
    
    * Address feedback on style and runtime cases
    
    * Update ArgType + 'k' prefix to all enums (#216)

--- src/PriceController.cpp
+++ src/PriceController.cpp
@@ -34,6 +34,12 @@ void PriceController::update_price(Plan * plan) {
         hours += plan->raw_materials - plan->raw_materials_remaining;
         workers += plan->workers.size();
     }
+    if (units <= 0) {
+        throw std::runtime_error("Units cannot be 0 or less for product: " + product->product_name); 
+    }
+    if (workers <= 0) {
+        throw std::runtime_error("Plan cannot be completed without workers: " + product->product_name); 
+    }
     double price = product->living_labor_per_unit = hours / units;
     double machine_use_hours = hours / workers;
     double machine_hours_per_unit = machine_use_hours / units;
