f24ba82ef9140fe61540c8fea4fa34f55c97be6c
Amittai Aviram <aviram@bc.edu>
2026-06-25 12:16:20 -0400

    Clean up Plan and Order, rename fields

--- header/Plan.h
+++ header/Plan.h
@@ -1,24 +1,18 @@
 #pragma once
 
 struct Plan {
-	// independent/input fields
 	Order * order;
     Firm * firm;
 	std::vector<Person *> workers;
-    unsigned int local_work_hours_daily;
-
-	// dependent/output fields	
-	double predicted_turnaround_time;
-    double machinery_cost;
-    double labor_hours;
-    double raw_materials;
-    double total_hours;
-    double prd;
-    double labor_hours_remaining;
-    double raw_materials_remaining;
-    double total_hours_remaining;
-    double quantity_remaining;
-	int outgoing_units_consumed;
+    unsigned int local_work_hours_daily = 0;
+	double predicted_turnaround_time = 0.0;
+    double machinery_budget = 0.0;
+    double labor_budget = 0.0;
+    double raw_materials_budget = 0.0;
+    double debt = 0.0;
+    double labor_hours_used = 0.0;
+    double quantity_remaining = 0.0;
+	int outgoing_units_consumed = 0;
 };
 
 
--- src/PriceController.cpp
+++ src/PriceController.cpp
@@ -42,7 +42,7 @@ void PriceController::update_price(Plan * plan) {
     for (std::pair<Plan *, int> entry : plan_history[product]) {
         Plan * plan = entry.first;
         units += plan->order->quantity - plan->quantity_remaining;
-        hours += plan->labor_hours - plan->labor_hours_remaining;
+        hours += plan->labor_hours_used;
         workers += plan->workers.size();
     }
     double price = product->living_labor_per_unit = hours / units;
