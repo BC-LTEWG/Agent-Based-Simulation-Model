264d6840be4001a1c586068584a94ff2b6548e9b
Alex Creiner <alexbcreiner@gmail.com>
2026-07-27 15:24:14 -0400

    about to try devins demand fix

--- src/Firm.cpp
+++ src/Firm.cpp
@@ -206,9 +206,6 @@ void Firm::check_and_reorder_input(Product * product) {
         LogPair("product_id", product->id),
         LogPair("resupply_deficit", resupply_deficit)
     );
-
-
-
     // double lead_time = std::min(
     //     get_inventory_level(product) / resupply_deficit,
     //     FIRM_STOCKPILE_DURATION * FIRM_REORDER_MAX_PROP
