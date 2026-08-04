0f9bb88f48e1da526f7010ff2cb29940c91c4ee1
Alex Creiner <alexbcreiner@gmail.com>
2026-08-03 22:30:28 -0400

    about to update overseer and begin making script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# current_dir="$(pwd)"
+
+# sed -i 's/paper_release_mode: false/paper_release_mode: true' ./Overseer/src/overseer/defaults/config.example.yml
+# echo "presets:\n   default_preset:\n   name: default_preset\n   params:\n   exe_path: $current_dir" > 
