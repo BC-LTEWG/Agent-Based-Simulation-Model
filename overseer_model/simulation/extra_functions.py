45ddbb20593e410e646a0a50f13eb313d9110f77
Alex Creiner <alexbcreiner@gmail.com>
2026-08-12 17:48:23 -0400

    updated overseer to make graph panel key errors warning log level, fixed some format_plot_config issues

--- overseer_model/simulation/extra_functions.py
+++ overseer_model/simulation/extra_functions.py
@@ -4,39 +4,49 @@ from numpy import long
 logger = logging.getLogger(__name__)
 from copy import deepcopy
 
+def set_labels(plot_dict, label_prefix, n_produced, n_machines):
+    labels = [f"{label_prefix} Sector P{i+1}" for i in range(n_produced)]
+    labels += [f"{label_prefix} Distribution"]
+    labels += [f"{label_prefix} Sector M{i+1}" for i in range(n_machines)]
+    plot_dict["labels"] = labels
+
 def format_plot_config(params, plotting_data):
     data = deepcopy(plotting_data)
 
-    n_produced = params.N_c
+    n_produced = params.N_g
     n_machines = params.N_m
 
-    employment_plots = data["employment"]["plots"]
-    sectoral_employment = employment_plots["sectoral_employment"]
-    long_run_employment = employment_plots["long_run_sectoral_employment"]
-    minimal_employment = employment_plots["equilibrium_employment"]
-
-    to_change = [sectoral_employment, long_run_employment, minimal_employment]
-    for plot_dict in to_change:
-        del plot_dict["label_template"]
+    to_change = {
+        "wealth": {
+            "sectoral_average_account": "Average Account in"
+        },
+        "employment": {
+            "sectoral_employment": "Workers Employed in",
+            "long_run_sectoral_employment": "Long-Run Employment in",
+            "equilibrium_employment": "Minimum Necessary Employment in",
+            "transfer_requests": "Transfer Requests from",
+            "surplus_labor_ratios": "Ratio of Minimal to Actual Employment in"
+        },
+        "busyness": {
+            "sectoral_busyness": "Busyness in"
+        },
+        "plans": {
+            "drafting_failures": "Hourly Drafting Failures from"
+        },
+        "activity": {
+            "sectoral_activity_levels": "Long Run Activity Level of",
+            "equilibrium_activity_levels": "Min Hourly Output in"
+        }
+    }
     
-    sectoral_employment["labels"] = [f"Workers Employed in Sector P{i}" for i in range(n_produced)]
-    sectoral_employment["labels"] += [f"Workers Employed in Distribution"]
-    sectoral_employment["labels"] += [f"Workers Employed in Sector M{i}" for i in range(n_machines)]
-
-    long_run_employment["labels"] = [f"Long-Run Employment in Sector P{i}" for i in range(n_produced)]
-    long_run_employment["labels"] += [f"Long-Run Employment in Distribution"]
-    long_run_employment["labels"] += [f"Long-Run Employment in Sector M{i}" for i in range(n_machines)]
-
-    minimal_employment["labels"] = [f"Minimum Necessary Employment in Sector P{i}" for i in range(n_produced)]
-    minimal_employment["labels"] += [f"Minimum Necessary Employment in Distribution"]
-    minimal_employment["labels"] += [f"Minimum Necessary Employment in Sector M{i}" for i in range(n_machines)]
-   
-    busyness_plots = data["busyness"]["plots"]
-    sectoral_busyness = busyness_plots["sectoral_busyness"]
-    if sectoral_busyness.get("label_template") is not None:
-        del sectoral_busyness["label_template"]
-    sectoral_busyness["labels"] = [f"Busyness in Sector P{i}" for i in range(n_produced)]
-    sectoral_busyness["labels"] += [f"Busyness in Distribution Sector"]
-    sectoral_busyness["labels"] += [f"Busyness in Sector M{i}" for i in range(n_machines)]
+    for cat_key in to_change:
+        if cat_key not in data:
+            continue
+        plots_dict = data[cat_key]["plots"]
+        for plot_to_change, prefix in to_change[cat_key].items():
+            actual_plot_dict = plots_dict[plot_to_change]
+            if "label_template" in actual_plot_dict:
+                del actual_plot_dict["label_template"]
+            set_labels(actual_plot_dict, prefix, n_produced, n_machines)
 
     return data
